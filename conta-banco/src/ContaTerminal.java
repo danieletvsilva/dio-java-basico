ff02514c38c8d5bea0eb69644bfb8a11eb4723e7
Daniele Torres <danieletvsilva@gmail.com>
2023-08-22 10:46:27 -0300

    desafio conta bancária

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 0;
+        String agencia;
+        String nomeCliente;
+        double saldo = 250.00;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá! Digite seu nome: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Qual o número da sua agência: ");
+        agencia = scanner.nextLine();
+
+        System.out.println("E o número da conta: ");
+        numero = scanner.nextInt();
+
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco! Sua agência é %s e seu saldo de R$%.2f já está disponível para saque.",
+                nomeCliente, agencia, saldo);
     }
 }
